23808e08ba830db0ad97e6a6284138f5a0235955
hassan <hassanajibade17@gmail.com>
2023-08-26 06:39:12 +0100

    starting about

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Header from './Components/Header'
 import './SCSS/Style.scss'
 import './SCSS/Mobile.scss'
 import Home from './Components/Home'
+import About from './Components/About'
 
 const App = () => {
   return (
-    <div>
+    <div className='App_container'>
       <Header/>
       <Home/>
+      {/* <About/> */}
     </div>
   )
 }
---
+++ src/Components/About.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <main className='About_container'>
+            <span>
+            <h1>ABOUT ME</h1>
+            <div className='About_underline'></div>
+            <p>Here you will find more information about me, what I do, and my current skills mostly
+                 in terms of programming and technology.</p>
+                 </span>
+                 <section className='content_section'>
+                    <div className='About_writeup'>
+                        <h2>Get To Know Me</h2>
+                    </div>
+                    <div className='My_skills'>
+
+                    </div>
+                 </section>
+        </main>
+    </div>
+  )
+}
+
+export default About
